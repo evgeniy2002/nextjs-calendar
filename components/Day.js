8b2985ae8cd384b54e7a4691bb4872a59114ded4
Evgenij Kobzev <you@gmail.com>
2023-02-13 01:45:42 +0300

    new changes

--- components/Day.js
+++ components/Day.js
@@ -19,7 +19,6 @@ export const Day = ({ day, rowIdx }) => {
       ? 'bg-blue-600 text-white rounded-full w-7 py-1'
       : '';
   };
-  console.log(dayEvents);
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
